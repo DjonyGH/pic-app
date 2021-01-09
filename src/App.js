7052c560b222291ccbdf1da487edcb6defc889ef
Евгений Калимуллин <counsil1986@gmail.com>
2021-01-09 20:14:17 +0700

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
+import { useState, useEffect } from 'react';
 import styles from './App.module.css';
-
-import Card from './components/Card'
+import Card from './components/Card';
 
 function App() {
+  const [photo, setPhoto] = useState();
+  // const [userName, setUserName] = useState();
+  // const [photoUrlSm, setPhotoUrlSm] = useState();
+  // const [photoDate, setPhotoDate] = useState();
+  // const [photoLikes, setPhotoLikes] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://api.unsplash.com/photos?client_id=B12PstzUWrwhlWL7XUlHpblLuXqHbtBPBJBkaV5Kh_Q');
+      const data = await res.json();
+      console.log(data);
+      // setUserName(data.user.name);
+      // setPhotoUrlSm(data.urls.small);
+      // setPhotoDate(data.created_at.split('T')[0]);
+      // setPhotoLikes(data.likes);
+      setPhoto(data[0]);
+    }
+    fetchData();
+  });
+  
   return (
     <div className={styles.app}>
       <div className={styles.cardsWrapper}>
         <Card 
-          userName="djony"
-          urlImg="/img/forest.jpg"
-          dateImg="12.12.2020"
-          countLikes="500"
+          // userName={userName}
+          // urlImg={photoUrlSm}
+          // dateImg={photoDate}
+          // countLikes={photoLikes}
+          userName={photo.user.name}
         />
       </div>
     </div>
