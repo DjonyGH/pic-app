2d90d02ba31fe0086cdf634a1782bdc195c94381
Евгений Калимуллин <counsil1986@gmail.com>
2021-01-24 12:29:51 +0700

    added button loadMore and link to user profile

--- src/App.js
+++ src/App.js
@@ -4,23 +4,36 @@ import Card from './components/Card';
 
 function App() {
   const [photos, setPhotos] = useState([]);
+  const [countPerPages, setCountPerPages] = useState(10);
+
+
 
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch('https://api.unsplash.com/photos?client_id=B12PstzUWrwhlWL7XUlHpblLuXqHbtBPBJBkaV5Kh_Q');
+      const res = await fetch(`https://api.unsplash.com/photos?per_page=${countPerPages}&client_id=B12PstzUWrwhlWL7XUlHpblLuXqHbtBPBJBkaV5Kh_Q`);
       const data = await res.json();
       console.log(data);
       setPhotos(data);
     }
     fetchData();
-  }, []);
+  }, [countPerPages]);
   
   return (
     <div className={styles.app}>
+      <div className={styles.header}>
+        <button
+          className={styles.loadMore}
+          onClick={() => setCountPerPages(countPerPages + 10)}
+        >
+          Загрузить еще
+        </button>
+      </div>
       <div className={styles.cardsWrapper}>
         {photos.map(photo => (
           <Card
+            key={photo.id}
             userName={photo.user.name}
+            userUrl={photo.user.links.html}
             urlImg={photo.urls.small}
             dateImg={photo.created_at.split('T')[0]}
             countLikes={photo.likes}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import styles from './card.module.css';
 
 function Card(props) {
-    const {userName, urlImg, dateImg, countLikes} = props;
+    const {userName, userUrl, urlImg, dateImg, countLikes} = props;
     return (
         <div className={styles.card}>
             <div className={styles.userName}>
-                {userName}
+                <a href={userUrl} className={styles.userUrl} rel="noreferrer" target="_blank">{userName}</a>
             </div>
             <img className={styles.picSmall} src={urlImg} alt="pic"/>
             <div className={styles.info}>
